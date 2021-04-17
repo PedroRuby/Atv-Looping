7133936d5ed9a238d2d4a38a70b318992be12524
PedroRuby <82732887+PedroRuby@users.noreply.github.com>
2021-04-17 18:24:34 -0300

    Create Ex3.swift

---
+++ Ex3.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+for i in 1...10 {
+    let notas: (Int, Int, Int)
+  
+    print("Aluno \(i):")
+    print("\tNota 1: ", terminator: "")
+    notas.0 = (readLine()! as NSString).integerValue
+    print("\tNota 2: ", terminator: "")
+    notas.1 = (readLine()! as NSString).integerValue
+    print("\tNota 3: ", terminator: "")
+    notas.2 = (readLine()! as NSString).integerValue
+    
+    let media = (notas.0 * 2 + notas.1 * 4 + notas.2 * 3) / 10
+    print("\tMedia: \(media)\n")
+}
